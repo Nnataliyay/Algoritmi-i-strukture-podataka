b48ad14884cb13f234814831b6cb280b4a38fbc2
Natalija Jolovic <85318996+Nnataliyay@users.noreply.github.com>
2023-11-28 23:52:37 +0100

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.spa_vezbe_5_1;
+
+/**
+ *
+ * @author NATALIJA
+ */
+public class MergeSort {
+    
+}
---
+++ SPA_vezbe_5_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.spa_vezbe_5_1;
+
+/**
+ *
+ * @author NATALIJA
+ */
+public class SPA_vezbe_5_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
