cf5d79ca5b905cc84dc3a037ff8a1488e318b65e
Natalija Jolovic <85318996+Nnataliyay@users.noreply.github.com>
2023-11-29 00:33:42 +0100

    Add files via upload

--- Algoritmi/05/MergeSort.java
+++ Algoritmi/05/MergeSort.java
@@ -1,13 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.spa_vezbe_5_1;
 
-/**
- *
- * @author NATALIJA
- */
+
 public class MergeSort {
     
+    void printArr(int[] arr){
+        for(int i: arr){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
+    
+    int[] mergeSort(int arr[]){
+        
+        if(arr.length <= 1){
+            return arr;
+        }
+        
+        int midpoint = arr.length/2;
+        
+        int[] levi = new int[midpoint];
+        int[] desni;
+        
+        if(arr.length %2 == 0){
+            desni = new int[midpoint];
+        }else{
+            desni = new int[midpoint + 1];
+        }
+        
+        for(int i = 0; i < midpoint; i++){
+            levi[i] = arr[i];
+        }
+        for(int j = 0; j < desni.length; j++){
+            desni[j] = arr[midpoint + j];
+        } 
+        
+        int[] rezultat = new int[arr.length];
+        
+        levi = mergeSort(levi);
+        desni = mergeSort(desni);
+        rezultat = merging(levi,desni);
+        
+        return rezultat;
+       
+    }
+    
+    int[] merging(int[] levi, int[] desni){
+        int[] rezultat = new int[levi.length + desni.length];
+        
+        int leviPokazivac, desniPokazivac, rezultatPokazivac;
+        
+        leviPokazivac = desniPokazivac = rezultatPokazivac = 0;
+        
+        while(leviPokazivac < levi.length || desniPokazivac < desni.length){
+            if(leviPokazivac < levi.length && desniPokazivac < desni.length){
+                if(levi[leviPokazivac] <= desni[desniPokazivac]){
+                    rezultat[rezultatPokazivac++] = levi[leviPokazivac++];
+                    
+                }else{
+                    rezultat[rezultatPokazivac++] = desni[desniPokazivac++];
+                }
+            }else if(leviPokazivac < levi.length){
+                rezultat[rezultatPokazivac++] = levi[leviPokazivac++];
+            }else if(desniPokazivac < desni.length){
+                rezultat[rezultatPokazivac++] = desni[desniPokazivac++];
+            }
+            
+        }
+        
+        return rezultat;  
+    }
 }
--- Algoritmi/05/SPA_vezbe_5_1.java
+++ Algoritmi/05/SPA_vezbe_5_1.java
@@ -1,16 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.spa_vezbe_5_1;
 
-/**
- *
- * @author NATALIJA
- */
+
 public class SPA_vezbe_5_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        MergeSort ms = new MergeSort();
+        
+        int[] arr = {4, 12, 16, 1, 5, 22 , 2};
+        
+        System.out.println("Originalan niz: ");
+        ms.printArr(arr);
+        
+        arr = ms.mergeSort(arr);
+        System.out.println("Sortirani niz: ");
+        ms.printArr(arr);
     }
 }
